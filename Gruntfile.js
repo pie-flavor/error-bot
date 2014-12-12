b34a67c21bc25fba6958dd21d745ec4ff685de11
error <error@error-unlimited.net>
2014-12-12 11:17:26 -0600

    add build files

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		tsdcfg: grunt.file.readJSON( 'tsd.json' ),
+		opt: {
+			srcDir: 'src',
+			outDir: 'out',
+			defDir: '<%= tsdcfg.path %>',
+			referenceDef: '<%= opt.defDir %>/reference.d.ts'
+		},
+		clean: [
+			'<%= opt.outDir %>//*',
+			'<%= tsdcfg.bundle %>',
+			'<%= opt.referenceDef %>'
+		],
+		tsd: {
+			refresh: {
+				options: {
+					command: 'reinstall',
+					latest: true,
+					overwrite: true,
+					config: './tsd.json'
+				}
+			}
+		},
+		ts: {
+			options: {
+				fast: 'never',
+				compile: true,
+				comments: true,
+				target: 'es5',
+				module: 'commonjs',
+				noImplicitAny: false,
+				sourceRoot: '',
+				maproot: '',
+				declaration: false,
+				verbose: true,
+				failOnTypeErrors: false
+			},
+			default: {
+				src: [ '<%= opt.srcDir %>/**/*.ts', '!<%= opt.srcDir %>/**/*.d.ts' ],
+				outDir: '<%= opt.outDir %>',
+				reference: '<%= referenceDef %>'
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-tsd' );
+	grunt.loadNpmTasks( 'grunt-ts' );
+
+	grunt.registerTask( 'default', [ 'clean', 'tsd', 'ts' ] );
+};
