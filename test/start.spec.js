f38c28cc4ad329f9898d00cecff7d51f06d30ecf
error <error@error-unlimited.net>
2016-06-07 21:17:34 -0500

    add tests

--- test/start.spec.js
+++ test/start.spec.js
@@ -9,15 +9,44 @@ const { describe, before, beforeEach, after, afterEach, it } = require( 'mocha' 
 chai.use( require( 'sinon-chai' ) );
 
 describe( 'start', () => {
-	let startSpy = spy(),
-		errorBotStub = stub().returns( { start: startSpy } );
-	before( () => {
-		proxyquire( '../out/start', { './error-bot': { default: errorBotStub, ErrorBot: errorBotStub } } );
+	let errorBotCtor,
+		errorBot,
+		startResult = Promise.resolve();
+	beforeEach( () => {
+		errorBot = { start: stub().returns( startResult ) };
+		errorBotCtor = stub().returns( errorBot );
+		stub( process, 'exit' );
+		stub( console, 'error' );
+		proxyquire( '../out/start', { './error-bot': { default: errorBotCtor, ErrorBot: errorBotCtor } } );
+	} );
+	afterEach( () => {
+		console.error.restore();
+		process.exit.restore();
 	} );
 	it( 'calls ErrorBot constructor', () => {
-		expect( errorBotStub ).to.have.been.calledWithNew;
+		expect( errorBotCtor ).to.have.been.calledOnce.calledWithNew;
 	} );
 	it( 'calls the start method once', () => {
-		expect( startSpy ).to.have.been.calledOnce;
+		expect( errorBot.start ).to.have.been.calledOnce;
+	} );
+	describe( 'success', () => {
+		it( 'exits the process with code 0', () => {
+			expect( process.exit ).to.have.been.calledOnce.calledWith( 0 );
+		} );
+		it( 'does not log any errors to the console', () => {
+			expect( console.error ).not.to.have.been.called;
+		} );
+	} );
+	describe( 'failure', () => {
+		let error = new Error( 'foo bar' );
+		before( () => {
+			startResult = Promise.reject( error );
+		} );
+		it( 'exits the process with code 1', () => {
+			expect( process.exit ).to.have.been.calledOnce.calledWith( 1 );
+		} );
+		it( 'logs error to the console', () => {
+			expect( console.error ).to.have.been.calledOnce.calledWith( error );
+		} );
 	} );
 } );
