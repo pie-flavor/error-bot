3d19a06fc848d51ed3bca935561746981fcd3472
error <error@error-unlimited.net>
2016-06-09 07:23:08 -0500

    add client code

--- src/error-bot.ts
+++ src/error-bot.ts
@@ -1,5 +1,74 @@
+import * as io from 'socket.io-client';
+
+import NodeBBApi from './nodebb-api';
+
+import { wait } from './time';
+
+const wildcard = require('socketio-wildcard')( io.Manager );
+
 export default class ErrorBot {
 	public async start() {
-		throw new Error( 'Not implemented' );
+		return new Promise( async ( resolve, reject ) => {
+			const { baseUrl } = require( '../data/config.json' );
+			console.log( `Connecting to ${baseUrl}...` );
+			const socket = io( baseUrl );
+			wildcard( socket );
+
+			function emit( message: string, ...args ) {
+				return new Promise( ( resolve, reject ) => {
+					socket.emit( message, ...args, ( error, data ) => {
+						if( error ) {
+							reject( error );
+						} else {
+							resolve( data );
+						}
+					} );
+				} );
+			}
+
+			socket.on( 'disconnect', () => {
+				reject( new Error( 'Disconnected from server' ) );
+			} );
+
+			socket.on( '*', ( ...args ) => {
+				console.log( ...args );
+			} );
+
+			socket.on( 'event:new_notification', data => {
+				console.log( 'event:new_notification', data );
+			} );
+
+			await ( new Promise( resolve => {
+				socket.on( 'connect', () => { resolve(); } );
+			} ) );
+
+			try {
+				console.log( 'Connected' );
+
+				const api = new NodeBBApi( baseUrl ),
+					{ username, password } = require( '../data/auth.json' );
+
+				console.log( 'Logging in...' );
+				await api.logIn( username, password );
+				console.log( 'Logged in' );
+
+				console.log( 'Fetching config...' );
+				const config = await api.getConfig();
+				console.dir( config );
+
+				const notifications = await emit( 'notifications.get', null );
+				console.dir( notifications );
+
+				await wait( 60 * 1000 * 5 );
+
+				console.log( 'Logging out...' );
+				await api.logOut();
+				console.log( 'Logged out' );
+
+				resolve();
+			} catch( ex ) {
+				reject( ex );
+			}
+		} );
 	}
 }
---
+++ src/nodebb-api.ts
@@ -0,0 +1,91 @@
+import * as rp from 'request-promise';
+
+export interface NodeBBConfig {
+	environment: string;
+	relative_path: string;
+	version: string;
+	siteTitle: string;
+	browserTitle: string;
+	titleLayout: string;
+	showSiteTitle: boolean;
+	minimumTitleLength: number;
+	maximumTitleLength: number;
+	minimumPostLength: number;
+	maximumPostLength: number;
+	minimumTagsPerTopic: number;
+	maximumTagsPerTopic: number;
+	hasImageUploadPlugin: boolean;
+	useOutgoingLinksPage: boolean;
+	allowGuestSearching: boolean;
+	allowGuestUserSearching: boolean;
+	allowGuestHandles: boolean;
+	allowFileUploads: boolean;
+	allowTopicsThumbnail: boolean;
+	usePagination: boolean;
+	disableChat: boolean;
+	socketioTransports: string[];
+	websocketAddress: string;
+	maxReconnectionAttempts: number;
+	reconnectionDelay: number;
+	topicsPerPage: number;
+	postsPerPage: number;
+	maximumFileSize: number;
+	'theme:id': string;
+	'theme:src': string;
+	defaultLang: string;
+	loggedIn: boolean;
+	'cache-buster': string;
+	requireEmailConfirmation: boolean;
+	topicPostSort: string;
+	categoryTopicSort: string;
+	csrf_token: string;
+	searchEnabled: boolean;
+	bootswatchSkin: boolean;
+	'composer-default': {};
+	markdown: {
+		highlight: number;
+		theme: string;
+	}
+}
+
+export default class NodeBBApi {
+	public constructor( public baseUrl: string ) {}
+
+	private _csrf: string;
+	private jar = rp.jar();
+
+	public async getConfig() {
+		const { baseUrl, jar } = this,
+			uri = `${baseUrl}/api/config`,
+			method = 'GET',
+			json = true,
+			config: NodeBBConfig = await rp( { uri, method, json, jar } );
+		this._csrf = config.csrf_token;
+		return config;
+	}
+
+	private async ensureCsrf() {
+		if( this._csrf ) return;
+		await this.getConfig();
+	}
+
+	public async logOut() {
+		await this.ensureCsrf();
+		const { baseUrl, _csrf, jar } = this,
+			uri = `${baseUrl}/logout`,
+			method = 'POST',
+			body = { _csrf },
+			json = true;
+		return rp( { uri, method, body, json, jar } );
+	}
+
+	public async logIn( username: string, password: string ) {
+		await this.ensureCsrf();
+		const { baseUrl, _csrf, jar } = this,
+			uri = `${baseUrl}/login`,
+			method = 'POST',
+			body = { username, password, _csrf },
+			json = true;
+		return rp( { uri, method, body, json, jar } );
+	}
+}
