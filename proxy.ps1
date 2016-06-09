1bd6ccbadbaeb1234f698dc0ca7d158014c83d7d
error <error@error-unlimited.net>
2016-06-09 14:59:33 -0500

    update proxy shell script

--- proxy.ps1
+++ proxy.ps1
@@ -1,3 +1,25 @@
-Set-Item env:\http_proxy http://localhost:55555/
-Set-Item env:\https_proxy http://localhost:55555/
-Set-Item env:\NODE_TLS_REJECT_UNAUTHORIZED 0
+param( [string]$url )
+
+function Set-Env {
+	param( [string]$name, [string]$value )
+	$Private:path = "env:\$name"
+	Set-Item $Private:path $value
+}
+
+function Unset-Env {
+	param( [string]$name )
+	$Private:path = "env:\$name"
+	If( Test-Path $Private:path ) {
+		Remove-Item $Private:path
+	}
+}
+
+$env:http_proxy = $url
+$env:https_proxy = $url
+if( !$url ) {
+	$env:NODE_TLS_REJECT_UNAUTHORIZED = ""
+	Write-Output "Proxy unset"
+} else {
+	$env:NODE_TLS_REJECT_UNAUTHORIZED = "0"
+	Write-Output "Proxy set to $url"
+}
--- unproxy.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item env:\http_proxy
-Remove-Item env:\https_proxy
-Remove-Item env:\NODE_TLS_REJECT_UNAUTHORIZED
