903348325a603f6f52e93d53784391f301cd1a9d
error <error@error-unlimited.net>
2016-06-09 07:23:28 -0500

    add proxy powershell

---
+++ proxy.ps1
@@ -0,0 +1,3 @@
+Set-Item env:\http_proxy http://localhost:55555/
+Set-Item env:\https_proxy http://localhost:55555/
+Set-Item env:\NODE_TLS_REJECT_UNAUTHORIZED 0
---
+++ unproxy.ps1
@@ -0,0 +1,3 @@
+Remove-Item env:\http_proxy
+Remove-Item env:\https_proxy
+Remove-Item env:\NODE_TLS_REJECT_UNAUTHORIZED
