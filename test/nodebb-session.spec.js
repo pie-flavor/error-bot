256990a3cfd6a62047bb291e257bee74d4b75285
error <error@error-unlimited.net>
2016-06-20 14:42:12 -0500

    add nodebb-session spec

---
+++ test/nodebb-session.spec.js
@@ -0,0 +1,26 @@
+const { describe, before, beforeEach, after, afterEach, it } = require( 'mocha' ),
+	chai = require( 'chai' ),
+	{ expect } = chai,
+	{ mock, stub, spy } = require( 'sinon' ),
+	proxyquire = require( 'proxyquire' ),
+	request = require( 'request' ),
+	{ jar } = request,
+	{ default: NodeBBSession } = require( '../out/nodebb-session' );
+
+describe( 'NodeBBSession', () => {
+	let session;
+
+	beforeEach( () => {
+		spy( request, 'jar' );
+		session = new NodeBBSession;
+	} );
+
+	it( 'creates a new cookie jar', () => {
+		expect( request.jar ).to.have.been.calledOnce;
+		expect( session.jar ).to.be.instanceof( Object.getPrototypeOf( jar() ).constructor );
+	} );
+
+	afterEach( () => {
+		request.jar.restore();
+	} );
+} );
