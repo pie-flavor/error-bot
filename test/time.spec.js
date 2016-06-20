78c4985d427f1bb6342c3b086449f970a52460ec
error <error@error-unlimited.net>
2016-06-20 14:41:50 -0500

    fix time spec

--- test/time.spec.js
+++ test/time.spec.js
@@ -2,17 +2,22 @@ const { describe, before, beforeEach, after, afterEach, it } = require( 'mocha' 
 	chai = require( 'chai' ),
 	{ expect } = chai;
 
+function now() {
+	const [ sec, nanoSec ] = process.hrtime();
+	return sec * 1e3 + nanoSec / 1e6;
+}
+
 describe( 'time', () => {
 	const time = require( '../out/time' );
 	describe( 'wait', () => {
 		it( 'resolves after a certain number of milliseconds', done => {
 			const waitTime = 25,
-				[ , beforeTime ] = process.hrtime();
+				beforeTime = now();
 			time.wait( waitTime ).then(
 				() => {
-					const [ , afterTime ] = process.hrtime(),
+					const afterTime = now(),
 						deltaTime = afterTime - beforeTime;
-					expect( deltaTime / 1000 ).to.be.at.least( waitTime );
+					expect( deltaTime ).to.be.within( waitTime, waitTime + 100 );
 				}
 			).then( () => { done(); }, err => { done( err ); } );
 		} );
